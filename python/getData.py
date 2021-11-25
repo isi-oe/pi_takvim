9cdf1048a889103d69df70cf1fd9746da891641e
I-Oe <i.oe@outlook.de>
2021-11-25 17:50:39 +0100

    added new files

--- python/getData.py
+++ python/getData.py
@@ -1,35 +1,39 @@
-from typing import Type
-import requests
-import json
+import requests, json
 
-with open('data/keys.json', "r") as read_file:
-    data = json.load(read_file)
+#read http-url from api.txt
+with open("data/api.txt") as f:
+    url = requests.get(f.readline())
+text = url.text
+data = json.loads(text)
 
-f = open("data/data.json", "w") 
+file = open("data/data.json", 'w')
+keys = open("data/keys.json", 'r')
 
-f.write("{")
+json_data = json.load(keys)
 
-f.write("\n\t\"today\" : {\n")
+# example
+# print(data["vakitler"][0]["imsak"])
+
+#iterate keys.json and only print prayertimes marked with 1
+
 i = 0
-for key, value in data["today"].items():
-    if i < 6:
-        f.write("\t\t\"" + str(key) + "\"" + " : " + "\"" + requests.get("http://api.thingspeak.com/apps/thinghttp/send_request?api_key=" + str(value)).text + "\",\n")
+for key, value in json_data.items():
+    if value == 1:
         i += 1
-    else:
-        f.write("\t\t\"" + str(key) + "\"" + " : " + "\"" + requests.get("http://api.thingspeak.com/apps/thinghttp/send_request?api_key=" + str(value)).text + "\"\n")
-        i += 1
-f.write("\t},")
 
-f.write("\n\t\"tomorrow\" : {\n")
-i = 0
-for key, value in data["tomorrow"].items():
-    if i < 6:
-        f.write("\t\t\"" + str(key) + "\"" + " : " + "\"" + requests.get("http://api.thingspeak.com/apps/thinghttp/send_request?api_key=" + str(value)).text + "\",\n")
-        i += 1
-    else:
-        f.write("\t\t\"" + str(key) + "\"" + " : " + "\"" + requests.get("http://api.thingspeak.com/apps/thinghttp/send_request?api_key=" + str(value)).text + "\"\n")
-        i += 1
-f.write("\t}\n")
+j = 0
+file.write("{\n")
+for key, value in json_data.items():
+    if(value == 1):
+        x = data["vakitler"][1][key][0]["tarih"]
+        x = x.replace(x[:11], '')
+        x = x.replace(x[5:], '')
+        if(j < i-1):    
+            file.write("\t\"" + key + "\" : \"" + x + "\",\n")
+            j += 1
+        else:
+            file.write("\t\"" + key + "\" : \"" + x + "\"\n")
+file.write("}")
 
-f.write("}")
-f.close
+file.close
+keys.close
