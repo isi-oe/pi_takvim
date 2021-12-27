c740ef96f91ab263371f49c2bfa5d1102e75f8ac
I-Oe <i.oe@outlook.de>
2021-12-27 18:05:09 +0100

    changed some code

--- python/getData.py
+++ python/getData.py
@@ -11,58 +11,47 @@ keys = open("data/keys.json", 'r')
 
 json_data = json.load(keys)
 
-# example
-# print(data["vakitler"][0]["imsak"])
-
-# iterate keys.json and only print prayertimes marked with 1
-
 i = 0
 for key, value in json_data.items():
     if value == 1:
         i += 1
 
-j = 0
+k = 0
+l = 0
 file.write("{\n")
-file.write("\t\"yesterday\" : [\n")
-for key, value in json_data.items():
-    if(value == 1):
-        x = data["vakitler"][0][key][0]["tarih"]
-        x = x.replace(x[:11], '')
-        x = x.replace(x[5:], '')
-        if(j < i-1):    
-            file.write("\t\t\"" + key + "\" : \"" + x + "\",\n")
-            j += 1
-        else:
-            file.write("\t\t\"" + key + "\" : \"" + x + "\"\n")
-file.write("\t],\n")
-
-j = 0
-file.write("\t\"today\" : [\n")
-for key, value in json_data.items():
-    if(value == 1):
-        x = data["vakitler"][1][key][0]["tarih"]
-        x = x.replace(x[:11], '')
-        x = x.replace(x[5:], '')
-        if(j < i-1):    
-            file.write("\t\t\"" + key + "\" : \"" + x + "\",\n")
-            j += 1
-        else:
-            file.write("\t\t\"" + key + "\" : \"" + x + "\"\n")
-file.write("\t],\n")
-
-j = 0
-file.write("\t\"tomorrow\" : [\n")
-for key, value in json_data.items():
-    if(value == 1):
-        x = data["vakitler"][2][key][0]["tarih"]
-        x = x.replace(x[:11], '')
-        x = x.replace(x[5:], '')
-        if(j < i-1):    
-            file.write("\t\t\"" + key + "\" : \"" + x + "\",\n")
-            j += 1
-        else:
-            file.write("\t\t\"" + key + "\" : \"" + x + "\"\n")
-file.write("\t]\n")
+while(k < 4):
+    j = 0
+    if(k == 0):
+        file.write("\t\"yesterday\" : [\n")
+    if(k == 1):
+        file.write("\t\"today\" : [\n")
+    if(k == 2):
+        file.write("\t\"tomorrow\" : [\n")
+    if(k == 3):
+        file.write("\t\"times\" : [\n")
+    for key, value in json_data.items():
+        if(value == 1):
+            if(k < 3):
+                x = data["vakitler"][l][key][0]["tarih"]
+                x = x.replace(x[:11], '')
+                x = x.replace(x[5:], '')
+                if(j < i-1):    
+                    file.write("\t\t\"" + x + "\",\n")
+                    j += 1
+                else:
+                    file.write("\t\t\"" + x + "\"\n")
+            else:
+                if(j < i-1):    
+                    file.write("\t\t\"" + key + "\",\n")
+                    j += 1
+                else:
+                    file.write("\t\t\"" + key + "\"\n")
+    if(k < 3):
+        file.write("\t],\n")
+    else:
+        file.write("\t]\n")
+    l += 1
+    k += 1
 file.write("}")
 
 file.close
